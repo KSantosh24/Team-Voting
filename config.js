3d953d6fcce7bac0b2731f729066c8429e49a9aa
k.santosh <kadurisantosh@gmail.com>
2022-01-21 01:48:13 -0800

    cha

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
 
-};
-// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCC7Iy_UcOYI8AnI94qgXwxh47XoJtJcKo",
+    authDomain: "voting-app-ce51f.firebaseapp.com",
+    projectId: "voting-app-ce51f",
+    storageBucket: "voting-app-ce51f.appspot.com",
+    messagingSenderId: "754967143528",
+    appId: "1:754967143528:web:8addbb2f02190c1fd1c7dd",
+    measurementId: "G-7NML9Y4993"
+  };
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
